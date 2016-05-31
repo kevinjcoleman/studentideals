8aa44b5cda048e90437dd4ce720d100932f27d20
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2016-05-31 15:20:00 -0700

    Fix to prevent ungeocoded businesses from showing in results, forgot to save the previous one.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   def show
     @region = Region.find(params[:region_id])
     @category = SidCategory.includes(:businesses).find(params[:id])
-    @businesses_all = @category.businesses.within(5, :origin => @region).by_distance(:origin => @region)
+    @businesses_all = @category.businesses.geocoded.within(5, :origin => @region).by_distance(:origin => @region)
     @businesses = @businesses_all.page params[:page]
     add_breadcrumb @region.name, region_path(@region)
     add_breadcrumb @category.label, region_category_path(@region, @category)
