d26902b425809d3b71a9c8a9eb3c2a2d5c13e6cd
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2016-09-24 03:18:44 -0500

    Added in some scss changes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :find_categories_for_nav
+  before_action :find_defaults_for_nav
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def find_categories_for_nav
+  def find_defaults_for_nav
     @nav_categories = SidCategory.all
+    @nav_cities = Business.group("city, state").select("city, state, count(id) as count").order("count DESC").limit(5)
   end
 end
