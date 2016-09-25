0f64f116c9a29c690750b6d4769b4ffe8561fa7a
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2016-09-24 21:24:01 -0500

    Removed counts from all pages and re-did some of the headlines.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @categories = SidCategory.select('sid_categories.*, count(businesses.id) as business_count').joins('left outer join businesses on businesses.sid_category_id = sid_categories.id').group('sid_categories.id').order("business_count DESC")
+    @categories = SidCategory.all
     @states = Business.where("state IS NOT NULL").group("state").select("state, count(id) as count").order("count DESC").limit(5)
   end
 end
