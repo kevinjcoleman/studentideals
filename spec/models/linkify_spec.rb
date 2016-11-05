0234ff05a84da8bc82533178d1c8743714222db9
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2016-11-04 18:15:53 -0700

    fix for banner ad glitch.

--- spec/models/linkify_spec.rb
+++ spec/models/linkify_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Linkify, type: :model do
   let!(:link) {Linkify.new("Test content with link https://www.freecodecamp.com/challenges/center-text-with-bootstrap")}
+  
   describe ".parse" do 
     it "returns an array" do 
       expect(link.parse.class).to be Array
@@ -11,6 +12,4 @@ RSpec.describe Linkify, type: :model do
       expect(link.parse).to eq(%w(Test content with link https://www.freecodecamp.com/challenges/center-text-with-bootstrap))
     end
   end
-
-  
 end
