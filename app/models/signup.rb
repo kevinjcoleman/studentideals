d502cf90e84a862b7c9b501f15799d4188f192d9
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2016-12-17 13:35:02 -0800

    Add custom error messages.

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,6 +1,6 @@
 class Signup < ActiveRecord::Base
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
-  validates_presence_of :email
-  validates_format_of :email, with: EMAIL_REGEX
+  validates_presence_of :email, message: "You must provide an email."
+  validates_format_of :email, with: EMAIL_REGEX, message: "Email doesn't look like an email address."
 end
--- spec/models/signup_spec.rb
+++ spec/models/signup_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Signup, type: :model do
-  it { should validate_presence_of(:email) }
-
   context "#create" do
     it "should not allow improperly formatted emails" do
       expect(Signup.new(email: "student").valid?).to be_falsey
