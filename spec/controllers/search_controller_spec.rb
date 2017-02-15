23c741c5e75c1259f80704e8c3fe4d1360caa63f
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2017-02-14 23:58:39 -0800

    Completed redirect logic, need to fill out results page and corresponding controller action.

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe SearchController, type: :controller do
-
-  let!(:region) {create(:region, name: "test region")}
-  let!(:sid_category) {create(:sid_category, label: "test sid_category")}
-  let!(:sub_category) {create(:sub_category, label: "test sub_category", sid_category: sid_category)}
-  let!(:business) {create(:business, :with_ungeocoded_address, :with_lat_lng, biz_name: "test business")}
+  let(:region) {create(:region, name: "test region")}
+  let(:sid_category) {create(:sid_category, label: "test sid_category")}
+  let(:sub_category) {create(:sub_category, label: "test sub_category", sid_category: sid_category)}
+  let(:business) {create(:business, :with_ungeocoded_address, :with_lat_lng, biz_name: "test business")}
+  let(:blank_params) { {bizCat: nil,
+                         bizCatType: nil,
+                         location: nil,
+                         bizCatTerm: nil,
+                         currentLocationValue: nil}}
 
   describe "GET #results" do
     context "it's successful with random query" do
@@ -15,11 +19,6 @@ RSpec.describe SearchController, type: :controller do
   end
 
   describe "GET redirect" do
-    let!(:blank_params) { {bizCat: nil,
-                           bizCatType: nil,
-                           location: nil,
-                           bizCatTerm: nil,
-                           currentLocationValue: nil}}
     context "with region and bizCat" do
       let!(:region_params)  {blank_params.merge(location: region.id)}
 
@@ -76,25 +75,83 @@ RSpec.describe SearchController, type: :controller do
     end
 
     context "with region and bizCatTerm" do
+      let(:params) {blank_params.merge(location: region.id, bizCatTerm: "test")}
 
+      before { get :redirect, params }
+      it { should redirect_to search_results_path({region: region.id, bizCatTerm: "test"}) }
     end
 
     context "with both a currentLocationValue & bizCatTerm" do
+      let(:params) {blank_params.merge(currentLocationValue: "test", bizCatTerm: "test")}
 
+      before { get :redirect, params }
+      it { should redirect_to search_results_path({currentLocationValue: "test", bizCatTerm: "test"}) }
     end
 
     context "with just bizCatTerm" do
+      let(:params) {blank_params.merge(bizCatTerm: "test")}
 
+      before { get :redirect, params }
+      it { should redirect_to search_results_path({bizCatTerm: "test"}) }
     end
 
     context "with just a currentLocationValue" do
+      let(:params) {blank_params.merge(currentLocationValue: "test")}
 
+      before { get :redirect, params }
+      it { should redirect_to search_results_path({currentLocationValue: "test"}) }
     end
 
     context "with no values" do
       before { get :redirect, blank_params }
       it { should redirect_to root_path }
-      it { should set_flash[:danger].to('You must provide a search term.') }
+      it { should set_flash[:danger].to('You must provide a search term!') }
+    end
+  end
+
+  describe "#results" do
+    context "with region and bizCatTerm" do
+      let(:params) {blank_params.merge(location: region.id, bizCatTerm: "test")}
+
+      before { get :results, params }
+      it { should respond_with(:success) }
+
+      it "assigns the region with corresponding categories and businesses correctly" do
+
+      end
+    end
+
+    context "with both a currentLocationValue & bizCatTerm" do
+      let(:params) {blank_params.merge(currentLocationValue: "test", bizCatTerm: "test")}
+
+      before { get :results, params }
+      it { should respond_with(:success) }
+
+      it "assigns regions, categories and businesses correctly" do
+
+      end
+    end
+
+    context "with just bizCatTerm" do
+      let(:params) {blank_params.merge(bizCatTerm: "test")}
+
+      before { get :results, params }
+      it { should respond_with(:success) }
+
+      it "assigns just categories and businesses" do
+
+      end
+    end
+
+    context "with just a currentLocationValue" do
+      let(:params) {blank_params.merge(currentLocationValue: "test")}
+
+      before { get :results, params }
+      it { should respond_with(:success) }
+
+      it "assigns just regions" do
+
+      end
     end
   end
 
