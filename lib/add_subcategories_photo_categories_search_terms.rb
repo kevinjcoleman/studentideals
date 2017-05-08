1fdd5cd51b591037ecfffdab9a6ed8fb112ddda5
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2017-05-07 18:25:20 -0700

    Updated script for addding search terms.

--- lib/add_subcategories_photo_categories_search_terms.rb
+++ lib/add_subcategories_photo_categories_search_terms.rb
@@ -1,11 +1,11 @@
 require 'csv'
 
-CSV.foreach('lib/sid_additional_subcategories.csv', headers: true) do |row|
-  business = Business.find(row["biz_id"])
+CSV.foreach('lib/BizId_PhotoCategory_SearchTerms.csv', headers: true) do |row|
+  business = Business.find_by(biz_id: row["biz_id"])
   subcategory = SubCategory.find_by(photo_category: row["photo_category"])
   business.add_sub_category(subcategory)
-  business.update_attributes!(photo_category: subcategory)
+  business.update_attributes!(photo_category_id: subcategory.id)
   row['search_terms'].split(',').each do |term|
-    search_term = SearchTerm.where(term: term.strip).first_or_create
+    SearchTerm.add_search_term(term, business)
   end
 end
